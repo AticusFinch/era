a1ad2a9b131088770fdd078594cbd3d32ed7c9db
Stasa Bastrica <stasabastrica7@gmail.com>
2025-11-08 13:06:46 +0100

    footer

---
+++ app/components/pageUnderConstruction.js
@@ -0,0 +1,11 @@
+import styles from "./pageUnderConstruction.module.css";
+
+const PageUnderConstruction = () => {
+  return (
+    <div className={styles.pageUnderConstruction}>
+      <h1>Page Under Construction</h1>
+    </div>
+  );
+};
+
+export default PageUnderConstruction;
